860eedf55db25791be3480451dde263b28511b89
Emrys Hughes <emrys@Emryss-MacBook-Air.local>
2018-03-20 21:21:35 +1100

    draggable learning check working woohoo!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import styles from '../node_modules/dragula/dist/dragula.css';
 
 //import DataStore from './flux/DataStore.js';
 
@@ -8,7 +9,7 @@ import SARCourse from './components/GetData4.js';
 
 import Question from './components/Question';
 import Text from './components/Text';
-//import LearningCheck from './components/LearningCheck';
+import LearningCheck from './components/LearningCheck';
 import Video from './components/Video';
 
 
@@ -51,6 +52,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to the Survive Armed Robbery course</h1>
         </header>
+        
+        <LearningCheck intro="move stuff around brochacho" options={['me','myself','I']} />
+
         <Question content="Who is most likely to commit an armed robbery?" />
 
         <Video vimeo_code="182642621" video_intro_text="Video intro text" />
--- src/components/LearningCheck.js
+++ src/components/LearningCheck.js
@@ -1,29 +1,102 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import dragula from 'react-dragula';
-
-var itemsList = items.map(function(item){
-    return <li>{item}</li>;
-});
-
-var App = React.createClass({
-    options: { props.options };
+//import '../../node_modules/dragula/dist/dragula.css';
+import styles from "../../node_modules/dragula/dist/dragula.css";
+//import styles from '../../node_modules/dragula/example/example.css';
 
 
+/*
+var LearningCheck = React.createClass({
     render: function () {
+      return 
+      <div className="LearningCheck">
+        <p>{ props.intro }</p>
+        <div IdName="first">
+            <div>Swap me around</div>
+            <div>Swap them around</div>
+            <div>Swap the world around</div>
+        </div>
+        <div IdName="second">
+            <div>Swap us around</div>
+            <div>Swap things around</div>
+            <div>Swap everything around</div>
+        </div>
+      </div>;
+    },
+    componentDidMount: function () {
+      dragula([document.getElementById(first), document.getElementById(second)], {
+        revertOnSpill: true
+      });
+    }
+  });
+*/
+
+class LearningCheck extends React.Component {
+    static propTypes = {
+        intro: PropTypes.string.isRequired,
+        options: PropTypes.array
+    };
+    
+    render() {
+        return ( 
+            <div className="LearningCheck">
+                <p>{this.props.intro}</p>
+                <div Id="first">
+                <div>Swap me around</div>
+                <div>Swap them around</div>
+                <div>Swap the world around</div>
+            </div>
+            <div Id="second">
+                <div>Swap us around</div>
+                <div>Swap things around</div>
+                <div>Swap everything around</div>
+            </div>
+            </div>
+        );
+    };
+    componentDidMount () {
+        console.log('mounted!');
+        dragula([document.getElementById('first'), document.getElementById('second')], {
+            revertOnSpill: true
+        });
+    }
+}
+
+function shuffle(arr) {
+    var i,
+        j,
+        temp;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;    
+};
+
+/* 
+var half_length = Math.ceil(arrayName.length / 2);    
+
+var leftSide = arrayName.splice(0,half_length);
+
+function LearningCheck(props) {
+    var items = props.options;
+    var noItems = items.length;
+    var itemsList = items.map(function(item){
+      return <li>{item}</li>;
+    })
+    render function() {
         return  <div className="LearningCheck"><p>{ props.intro }</p><ul className="learning_check">{ itemsList }</ul></div>;
     },
     componentDidMount: function () {
-      var container = React.findDOMNode(this);
-      dragula([container]);
-    }
-  });
-  React.render(<App />, document.getElementById('examples'));
+        var container = React.findDOMNode(this);
+        dragula([container]);
+      }
+    
+} */
 
-LearningCheck.propTypes = {
-    intro: PropTypes.string.isRequired,
-    options: PropTypes.array
-  }
 
   export default LearningCheck;
